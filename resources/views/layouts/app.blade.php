7da5734bf597da80f6c9af9fb9aa9b0713cca6a5
nathaniafadlilah14 <nathaniafadlilah@student.telkomuniversity.ac.id>
2024-06-18 21:48:01 +0700

    contoh yang udah diubah

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,12 @@
     <!doctype html>
     <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{ $pageTitle }}</title>
+        <title>contoh berubah</title>
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
@@ -14,6 +15,7 @@
         <!-- Scripts -->
         @vite('resources/sass/app.scss')
     </head>
+
     <body>
         @include('layouts.nav')
         <div id="app">
@@ -25,5 +27,6 @@
             </main>
         </div>
     </body>
+
     </html>
- @include('layouts.footer')
+    @include('layouts.footer')
